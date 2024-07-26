2626b349c78ee35c9ac603fb0d21322be368cd48
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-07-26 15:03:34 +0200

    Instrument `handle_tunnel_state_transition`

--- mullvad-daemon/src/lib.rs
+++ mullvad-daemon/src/lib.rs
@@ -1295,6 +1295,7 @@ impl Daemon {
         }
     }
 
+    #[tracing::instrument(skip_all)]
     async fn handle_command(&mut self, command: DaemonCommand) {
         use self::DaemonCommand::*;
         if self.tunnel_state.is_disconnected() {
