3b0f61cca9b6ae3825de21038abfc04ef97d8ae2
Markus Pettersson <markus.pettersson@mullvad.net>
2024-08-19 10:28:31 +0200

    Fix `CARGO_TARGET_DIR` not being accounted for in `test/scripts/build.sh`

--- test/scripts/build.sh
+++ test/scripts/build.sh
@@ -7,23 +7,25 @@ set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TEST_FRAMEWORK_ROOT="$SCRIPT_DIR/.."
-REPO_ROOT="$SCRIPT_DIR/../.."
+REPO_ROOT="$TEST_FRAMEWORK_ROOT/.."
+# Explicitly set `CARGO_TARGET_DIR` to unify output directory for binaries from the app workspace and the test workspace
+export CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-"$TEST_FRAMEWORK_ROOT/target"}
 
 # Build
 build_linux() {
     mkdir -p "$TEST_FRAMEWORK_ROOT/dist"
     # Build the test manager
     cargo build -p test-manager --release
-    cp "$TEST_FRAMEWORK_ROOT/target/release/test-manager" "$TEST_FRAMEWORK_ROOT/dist/"
+    cp "$CARGO_TARGET_DIR/release/test-manager" "$TEST_FRAMEWORK_ROOT/dist/"
 
     # Build the test runner
     "$SCRIPT_DIR/build-runner.sh" linux
-    cp "$TEST_FRAMEWORK_ROOT/target/x86_64-unknown-linux-gnu/release/test-runner" "$TEST_FRAMEWORK_ROOT/dist/"
-    cp "$TEST_FRAMEWORK_ROOT/target/x86_64-unknown-linux-gnu/release/connection-checker" "$TEST_FRAMEWORK_ROOT/dist/"
+    cp "$CARGO_TARGET_DIR/x86_64-unknown-linux-gnu/release/test-runner" "$TEST_FRAMEWORK_ROOT/dist/"
+    cp "$CARGO_TARGET_DIR/x86_64-unknown-linux-gnu/release/connection-checker" "$TEST_FRAMEWORK_ROOT/dist/"
 
     # Build mullvad-version
     cargo build --manifest-path="$REPO_ROOT/Cargo.toml" --release --bin mullvad-version
-    cp "$REPO_ROOT/target/release/mullvad-version" "$TEST_FRAMEWORK_ROOT/dist/"
+    cp "$CARGO_TARGET_DIR/release/mullvad-version" "$TEST_FRAMEWORK_ROOT/dist/"
 }
 
 build_linux
