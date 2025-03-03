2b205b8a218db156eb5dee711ff4ab67453665e2
Oliver <oliver@mohlin.dev>
2025-03-03 10:09:22 +0100

    Add string button reset

--- desktop/packages/mullvad-vpn/src/renderer/lib/styles/mixins/button-reset.ts
+++ desktop/packages/mullvad-vpn/src/renderer/lib/styles/mixins/button-reset.ts
@@ -1,3 +1,5 @@
+import { css } from 'styled-components';
+
 export const buttonReset = {
   border: 'none',
   padding: 0,
@@ -8,3 +10,14 @@ export const buttonReset = {
   lineHeight: 'inherit',
   cursor: 'default',
 } as React.CSSProperties;
+
+export const buttonResetString = css`
+  border: none;
+  padding: 0;
+  margin: 0;
+  font: inherit;
+  color: inherit;
+  text-align: inherit;
+  line-height: inherit;
+  cursor: default;
+`;
